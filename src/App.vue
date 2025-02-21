f99f819720c86a1aafcfb9fc2059307d4ad54348
fujii-ken <fujii.k.cgp@gmail.com>
2025-02-21 19:13:07 +0900

    adding GrammerQuiz.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import GrammarQuiz from './components/GrammarQuiz.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="contents_wrapper">
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <div class="components">
+      <HelloWorld msg="Vite + Vue" />
+      <GrammarQuiz />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
